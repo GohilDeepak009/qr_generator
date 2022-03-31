09d00b2ef0e023a9b6245ae75d8133532e3c9fc3
Gohil Deepak <B61309@mail.jiit.ac.in>
2022-03-31 18:49:26 +0530

    made code cleaner

--- qr.cpp
+++ qr.cpp
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define PORT 80
+#define IP_ADDR "5.9.243.188"
+#define RESPONSE_BUFFER 20480
+
 using namespace std;
 
 int main()
@@ -21,7 +25,7 @@ int main()
         cout << ":) [SOCKET CREATED]\n";
     }
 
-    struct sockaddr_in qr = {'\0', AF_INET, htons(80), {inet_addr("5.9.243.188")}};
+    struct sockaddr_in qr = {'\0', AF_INET, htons(PORT), {inet_addr(IP_ADDR)}};
 
     if (connect(sock, (struct sockaddr *)&qr, sizeof(qr)) < 0)
     {
@@ -35,13 +39,15 @@ int main()
 
     string base_request = "GET /";
     string data;
+    cout<<"Enter text : ";
     cin >> data;
     base_request = base_request + data + " HTTP/1.1\r\nHost:qrenco.de\r\nUser-Agent:curl/7.35.0\r\n\r\n";
     cout << base_request;
     send(sock, &base_request[0], base_request.length(), 0);
 
-    char response[20480];
-    recv(sock, &response[0], 20480, 0);
+    char response[RESPONSE_BUFFER];
+//    string response = "";
+    recv(sock, &response[0], RESPONSE_BUFFER, 0);
     cout << response << "\n";
 
     close(sock);
